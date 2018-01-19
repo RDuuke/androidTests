05f28ed8a753a4dd8e314ea2f708942c3cf8d356
sgallova <sofigava@gmail.com>
2018-01-19 14:45:40 -0500

    first commit

---
+++ app/src/main/java/co/edu/udea/medicina/examenpostgrado2018/webView.java
@@ -0,0 +1,13 @@
+package co.edu.udea.medicina.examenpostgrado2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class webView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
